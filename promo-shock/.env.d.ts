27d6f0371d5dc71eb6ca9f5e24dbb8984cc26019
Pavel Shapov <pavel.shapov@metalamp.io>
2024-02-14 17:18:05 +0600

    update: env variables

--- promo-shock/.env.d.ts
+++ promo-shock/.env.d.ts
@@ -6,6 +6,9 @@ declare global {
       NEXT_PUBLIC_WC_PROJECT_ID: string;
       NEXT_PUBLIC_BSC_CHAIN_ID: '56' | '97';
       NEXT_PUBLIC_BSC_RPC_URL: string;
+      NEXT_PUBLIC_BSC_TICKET_FACTORY_ADDRESS: string;
+      NEXT_PUBLIC_BSC_PROMO_FACTORY_ADDRESS: string;
+      NEXT_PUBLIC_ETHERSCAN_API_KEY: string;
     }
   }
 }
