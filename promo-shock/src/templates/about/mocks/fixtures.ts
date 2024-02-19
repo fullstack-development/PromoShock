f7893ab706b31b53a471db73ef8c89cd260bfd58
Oleg Volobuev <olegvolobuev785@gmail.com>
2024-02-19 14:34:59 +0300

    fix: es-lint fix

--- promo-shock/src/templates/about/mocks/fixtures.ts
+++ promo-shock/src/templates/about/mocks/fixtures.ts
@@ -1,22 +1,20 @@
-import streamPreviewMock from "../../landing/mocks/stream-preview-mock.png";
-
 export const TEAM_LIST_MOCK = [
   {
     title: "Nikolay Bordunenko",
     description:
       "A crypto enthusiast and a devoted champion of the blockchain world, staying abreast of every industry advancement!",
-    preview: streamPreviewMock.src,
+    preview: "",
   },
   {
     title: "Nikolay Bordunenko",
     description:
       "A crypto enthusiast and a devoted champion of the blockchain world, staying abreast of every industry advancement!",
-    preview: streamPreviewMock.src,
+    preview: "",
   },
   {
     title: "Nikolay Bordunenko",
     description:
       "A crypto enthusiast and a devoted champion of the blockchain world, staying abreast of every industry advancement!",
-    preview: streamPreviewMock.src,
+    preview: "",
   },
 ];
